c4bdb06a81f5b4074579066f78eddec759805f92
Muhammed adediran <adediranmuhammed933@gmail.com>
2025-09-07 12:14:58 +0000

    Rectify all the bugs

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore";
 import { initializeAppCheck, ReCaptchaV3Provider } from "firebase/app-check";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDFSMKNCije34uXLMxFnEx00tbTwY08MWI",
-  authDomain: "hdtc-solution.firebaseapp.com",
-  projectId: "hdtc-solution",
-  storageBucket: "hdtc-solution.appspot.com",
-  messagingSenderId: "1070343300608",
-  appId: "1:1070343300608:web:0ca044f6cbe9065f98dae5",
-  measurementId: "G-PHYSKTE6Y4"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
